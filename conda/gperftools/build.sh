0bd6e12005f512a6747f906ef90e51f0ddd7cae2
Jason M Miller <Jason.Miller@inl.gov>
2022-05-25 14:27:52 -0600

    Successfully built Mac package

--- conda/gperftools/build.sh
+++ conda/gperftools/build.sh
@@ -4,13 +4,17 @@ set -ex
 
 # Get an updated config.sub and config.guess
 cp $BUILD_PREFIX/share/gnuconfig/config.* .
-./configure  --prefix $PREFIX --enable-libunwind --enable-frame-pointers
+export CC=$(mpicc -show | cut -d\  -f1) CXX=$(mpicxx -show | cut -d\  -f1)
+if [ `uname` == "Darwin" ]; then
+    ./configure --prefix $PREFIX --enable-frame-pointers
+else
+    ./configure --prefix $PREFIX --enable-libunwind --enable-frame-pointers
+fi
 CORES=${MOOSE_JOBS:-2}
 make -j $CORES
 make install
 # Remove unwanted pprof
 mv $(which pprof) $(dirname $(which pprof))/original_pprof
-
 # Build/Install pprof from google
 export CC=$(mpicc -show | cut -d\  -f1)
 export CXX=$(mpicxx -show | cut -d\  -f1)
