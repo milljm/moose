b7fb96f7ec7eeb0a8a14bf4bf24b1713b9ddac59
Jason M Miller <Jason.Miller@inl.gov>
2025-06-06 14:23:59 -0600

    match correctly

--- scripts/create_release.py
+++ scripts/create_release.py
@@ -13,8 +13,9 @@ import os
 import re
 import argparse
 import configparser
+import datetime
 from collections import namedtuple
-from jinja2 import Environment, FileSystemLoader
+from jinja2 import Environment, DictLoader
 try:
     import requests # type: ignore # for exceptions
 except ModuleNotFoundError:
@@ -26,6 +27,12 @@ try:
 except ModuleNotFoundError:
     print('Could not load `pygithub` module. Please install the GitHub Python API module.')
     sys.exit(1)
+
+# ASCII Color codes
+RED    = '\033[91m'
+GREEN  = '\033[92m'
+BOLD   = '\033[1m'
+RESET  = '\033[0m'
 
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 MOOSE_DIR = os.environ.get('MOOSE_DIR',
@@ -45,6 +52,9 @@ TEMPLATE_ENVS = {'APPLICATION'      : 'name of the application',
                  'MAIN_URL'         : 'URL to Civet displaying testing results for main branch',
                  'PROJECT_URL'      : 'URL to projects GitHub page',
                  'CIVET_RECIPE_SHA' : 'Civet Recipe SHA at the time of testing',
+                 'SUBMODULES'       : 'Submodules and their SHA',
+                 'SYNTAX_NAME'      : f'{RED}<YourName>{RESET}',
+                 'CIVET_VERSION'    : 'CIVETs version at the time of CIVET_RECIPE_SHA'
                  }
 
 # Syntax helper
@@ -52,12 +62,6 @@ parser = argparse.ArgumentParser(description='Output links associated with CI/CD
                                  epilog=f'Example usage: {os.path.basename(__file__)} '
                                         '-t /path/to/token'
                                         f' github.com/idaholab/moose abc123')
-
-# ASCII Color codes
-RED    = '\033[91m'
-GREEN  = '\033[92m'
-BOLD   = '\033[1m'
-RESET  = '\033[0m'
 
 class GitHubAPI():
     """ handle GH calls """
@@ -127,6 +131,8 @@ class CreateRelease():
     """
     def __init__(self, args):
         self.gh = GitHubAPI(args)
+        cgh = Github()
+        self.civet_repo = cgh.get_repo('idaholab/civet')
         self.commit = self.gh.get_commit()
         self.repo = self.gh.get_repo()
         self.sha = args.sha
@@ -191,26 +197,30 @@ class CreateRelease():
         commit_status = namedtuple('CommitStatus', ('state',
                                                     'context',
                                                     'target_url',
-                                                    'description'))
+                                                    'description',
+                                                    'updated_at'))
         tests = {}
         for status in status_tuples:
             if status.context not in tests:
                 tests[status.context] = commit_status(status.state,
                                                       status.context,
                                                       status.target_url,
-                                                      status.description)
+                                                      status.description,
+                                                      status.updated_at)
                 continue
             if tests[status.context] == commit_status(status.state,
                                                       status.context,
                                                       status.target_url,
-                                                      status.description):
+                                                      status.description,
+                                                      status.updated_at):
                 continue
             # This is our duplicate invalidated test. Assume 'group' status.
             override_status = f'{"success" if final_status else "failure"}'
             tests[status.context] = commit_status(override_status,
                                                   status.context,
                                                   status.target_url,
-                                                  status.description)
+                                                  status.description,
+                                                  status.updated_at)
 
         # return the same object type we started with
         return list(value for value in tests.values())
@@ -244,17 +254,47 @@ class CreateRelease():
         """ BOOL if every status is successful """
         return self.combined_status.state == 'success'
 
-def print_statuses(statuses: list[tuple[str: 'state',
-                                        str: 'context',
-                                        str: 'target_url',
-                                        str: 'description']],
-                   modules: list[tuple[str: 'name', str: 'path', str: 'sha']],
-                   civet_url: str,
-                   github_url: str):
-    """ print the statuses to stdout """
-    recipe_re = re.compile(r'recipe:([0-8a-f]+),')
-    # edge cases make up the initial tests (PRs) and the final (main)
+    def get_civet_version(self, date):
+        """ return CIVET version based on date of release """
+        return self.civet_repo.get_commits(until=date)[0].sha
 
+def get_recipe_sha(group: int, statuses: list[tuple[str: 'state',
+                                                    str: 'context',
+                                                    str: 'target_url',
+                                                    str: 'description',
+                                                    str: 'updated_at']])->str:
+    """
+    Return Civet recipe SHA for given group in statuses. While not 100% accurate,
+    the returned SHA will be the last recipe returned. See 'get_test_groups' for
+    an understanding on groups.
+    """
+    recipe_re = re.compile(r'recipe:([0-9a-f]+),')
+    groups = [statuses[:1]]
+    if statuses[1:-1] == statuses[-1:]:
+        groups.append(statuses[-1:])
+    else:
+        groups.extend([statuses[1:-1], statuses[-1:]])
+    recipe_sha = recipe_re.findall(groups[group][0][1][0].description)
+    recipe_sha = ''.join(recipe_sha) if recipe_sha else 'N/A'
+    return recipe_sha
+
+def get_test_groups(statuses: list[tuple[str: 'state',
+                                         str: 'context',
+                                         str: 'target_url',
+                                         str: 'description',
+                                         str: 'updated_At']])->tuple[list[str],list[tuple]]:
+    """
+    Statuses contains a list of lists of GitHub status tuples. Each list consists of
+    a group of tests performed by a branch. In the case of MOOSE, with a tiered CI system:
+    [[main],[devel],[next],[pr]] means: [1:-1] is the 'middle' (devel, next, etc) therefor
+    edge cases will be main [:1] and PRs [-1:].
+
+    This method returns two lists of identical sizes. One, the statuses, the other, the
+    branch name/identifier:
+
+    return (['MAIN/MASTER', 'DEVEL/NEXT', 'DEVEL/NEXT', 'PR'],
+           [[Status for MAIN],[Status for Devel],[Status for Next],[Status for PR]])
+    """
     groups = [statuses[:1]]
     labels = ['MAIN/MASTER:']
     # if project only has 'PR --> MAIN'
@@ -264,7 +304,56 @@ def print_statuses(statuses: list[tuple[str: 'state',
     else:
         groups.extend([statuses[1:-1], statuses[-1:]])
         labels.extend(['DEVEL/NEXT:', 'PR:'])
+    return (labels, groups)
 
+def create_release(statuses: list[tuple[str: 'state',
+                                        str: 'context',
+                                        str: 'target_url',
+                                        str: 'description',
+                                        str: 'updated_at']],
+                   modules: list[tuple[str: 'name', str: 'path', str: 'sha']],
+                   civet_url: str,
+                   cr: CreateRelease,
+                   **kwargs)->None:
+    """ print the statuses to stdout """
+    with open(TEMPLATE_FILE, 'r', encoding='utf-8') as f:
+        contents = f.read()
+
+    env = Environment(loader = DictLoader({'' : contents}),
+                      trim_blocks=True,
+                      lstrip_blocks=True)
+    modules_str = ''
+    if modules:
+        max_length = max(len(module.name) for module in modules) + 3
+        for module in modules:
+            modules_str += f'\t\t{module.name.ljust(max_length)}{module.sha[-8:]}\n'
+
+    meta_template = env.get_template('')
+    _, groups = get_test_groups(statuses)
+    release_date = groups[0][0][1][0].updated_at
+    civet_sha = cr.get_civet_version(release_date)
+    TEMPLATE_ENVS['APPLICATION'] = f'{GREEN}{os.path.basename(kwargs["uri"]).upper()}{RESET}'
+    TEMPLATE_ENVS['VERSION'] = f'{GREEN}{kwargs["create_release"]}{RESET}'
+    TEMPLATE_ENVS['DEVEL_URL'] = f'{GREEN}{civet_url}/{groups[1][0][0].sha[:8]}{RESET}'
+    TEMPLATE_ENVS['MAIN_URL'] = f'{GREEN}{civet_url}/{groups[0][0][0].sha[:8]}{RESET}'
+    TEMPLATE_ENVS['TEST_DATE'] = f'{GREEN}{release_date}{RESET}'
+    TEMPLATE_ENVS['PROJECT_URL'] = f'{GREEN}https://{kwargs["uri"]}{RESET}'
+    TEMPLATE_ENVS['CIVET_RECIPE_SHA'] = f'{GREEN}{get_recipe_sha(0, statuses)}{RESET}'
+    TEMPLATE_ENVS['CIVET_VERSION'] = f'{GREEN}{civet_sha}{RESET}'
+    TEMPLATE_ENVS['SUBMODULES'] = f'{GREEN}{modules_str}{RESET}'
+    meta_render = meta_template.render(TEMPLATE_ENVS)
+    print(f'\n\n{meta_render}')
+
+def print_statuses(statuses: list[tuple[str: 'state',
+                                        str: 'context',
+                                        str: 'target_url',
+                                        str: 'description',
+                                        str: 'updated_at']],
+                   modules: list[tuple[str: 'name', str: 'path', str: 'sha']],
+                   civet_url: str,
+                   github_url: str):
+    """ print the statuses to stdout """
+    labels, groups = get_test_groups(statuses)
     if modules:
         # Print submodules involved at their recorded hashes
         print(f'\n{BOLD}Submodule(s):{RESET}')
@@ -275,9 +364,8 @@ def print_statuses(statuses: list[tuple[str: 'state',
     # Begin to print statuses
     for label_index, group in enumerate(groups):
         _sha = [x[0] for x in group][0].sha
-        if group[0][1]:
-            recipe_sha = recipe_re.findall(group[0][1][0].description)
-            recipe_sha = ''.join(recipe_sha) if recipe_sha else 'N/A'
+        recipe_sha = get_recipe_sha(label_index, statuses)
+
         # CIVET does not record sha events for PRs
         events = (f'\n{BOLD}CIVET Event:{RESET} {civet_url}/{_sha[:8]}\n'
                   f'{BOLD}GitHub Event:{RESET} {github_url}/{_sha[:8]}'
@@ -291,8 +379,6 @@ def print_statuses(statuses: list[tuple[str: 'state',
             for test in tests:
                 print(f'\t{GREEN if test.state == "success" else RED}{test.state}{RESET}'
                       f' {test.context} {test.target_url}')
-    #print(f'\t{GREEN if test.state =="success" else RED}'
-    #    f'{test.content} {test.target_url}')
 
 def check_args(argv)->object:
     """ checks command line options """
@@ -335,7 +421,7 @@ def parse_args(argv)->object:
                         help='URI to GitHub repo. exp: github.com/idaholab/moose')
     parser.add_argument('sha', nargs='?', metavar='SHA',
                         help='Povided SHA to be considered for release')
-    parser.add_argument('-t','--token', nargs='?',
+    parser.add_argument('-t','--token',
                         help='Your GitHub API Token. Can either be an environment variable'
                         ' containing your token, or a path to a file containing your token, or '
                         ' the token itself. For security purposes, ideally this should be a path'
@@ -343,8 +429,8 @@ def parse_args(argv)->object:
                         ' https://github.com/settings/tokens')
     parser.add_argument('-p','--print-status', action='store_true', default=False,
                         help='Print all statuses relating to provided SHA')
-    parser.add_argument('-c','--create-release', action='store_true', default=False,
-                        help='Output a release document for provided SHA')
+    parser.add_argument('-c','--create-release',
+                        help='Output a release for SHA, and tag it with supplied version')
     parser.add_argument('-d','--debug', action='store_true', default=False,
                          help='Print API URL calls')
 
@@ -369,8 +455,12 @@ def main(args):
         print('\n\nIf you are satisfied with these results and would like to create a release '
               'please run the script again with -c|--create-release and follow the '
               'instructions.')
-    if args.create_release:
+    elif args.create_release:
+        print('Gathering submodule information...')
         modules = cr.get_submodules(cr.repo)
+        print('Gathering test information for Civet Recipe SHA, Test Date, etc...')
+        all_statuses = cr.get_statuses(cr.commit)
+        create_release(all_statuses, modules, civet_event, cr, **vars(args))
 
 if __name__ == '__main__':
     m_args = check_args(sys.argv[1:])
